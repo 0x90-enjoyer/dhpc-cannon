cc904228432af9b9bc2def1911cf9001c0dc095c
0x90-enjoyer <59457434+0x90-enjoyer@users.noreply.github.com>
2024-03-02 14:15:25 -0500

    Update botnet.sh

--- botnet.sh
+++ botnet.sh
@@ -23,9 +23,9 @@ fi
 # [OVERVIEW]
 
 # In this interactive lab, we will be exploring 3 of the 4 phases of the botnet lifecycle:
-# Infection (+ Propagation) -> Connection -> Control
+# Infection (+ Propagation) -> Connection -> Control.
 
-# We will not be covering Maintainence & Upgrading
+# We will not be covering Maintainence & Upgrading.
 
 
 # [REQUIREMENTS]
